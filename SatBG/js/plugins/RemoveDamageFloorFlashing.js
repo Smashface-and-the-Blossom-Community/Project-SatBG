8a0d6d1b0087881be9b0722029b2ebf570a35e9b
Kanmuri <Kanmuri@users.noreply.github.com>
2023-07-15 11:09:46 -0600

    Override hard-coded 10 damage per step on floor damage to be just 1 instead

--- SatBG/js/plugins/RemoveDamageFloorFlashing.js
+++ SatBG/js/plugins/RemoveDamageFloorFlashing.js
@@ -4,7 +4,7 @@
 //=============================================================================
 /*:
  * @target mz
- * @plugindesc Remove screen flashing from damage floors
+ * @plugindesc Remove screen flashing from damage floors, play a sound instead, and reduce damage to 1 instead of 10 per step
  * @author Kanmuri Aramaki
  */
 
@@ -14,5 +14,10 @@
         if (!$gameParty.inBattle()) {
             SoundManager.playActorDamage();
         }
+    };
+
+    //Override hard-coded floor damage of 10 per step to be just 1 instead
+    Game_Actor.prototype.basicFloorDamage = () => {
+        return 1;
     };
 })();
