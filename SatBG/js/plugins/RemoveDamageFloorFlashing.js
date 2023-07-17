db94b6b563729ca2305b822d5248209c42b38690
Kanmuri <Kanmuri@users.noreply.github.com>
2023-07-16 21:13:03 -0600

    Updated floor damage plugin to allow setting the amount of damage per step as a plugin parameter

--- SatBG/js/plugins/RemoveDamageFloorFlashing.js
+++ SatBG/js/plugins/RemoveDamageFloorFlashing.js
@@ -3,12 +3,22 @@
 // RemoveDamageFloorFlashing.js
 //=============================================================================
 /*:
- * @target mz
- * @plugindesc Remove screen flashing from damage floors, play a sound instead, and reduce damage to 1 instead of 10 per step
+ * @target MZ
+ * @plugindesc Remove screen flashing from damage floors, play a sound instead, and (by default) reduce damage to 1 instead of 10 per step
  * @author Kanmuri Aramaki
+ * 
+ * @param basicFloorDamage
+ * @text Basic Floor Damage
+ * @desc The base amount of damage per step from walking on a damage floor
+ * @type number
+ * @default 1
  */
 
 (() => {
+    const pluginName = 'RemoveDamageFloorFlashing';
+    const parameters = PluginManager.parameters(pluginName);
+    const basicFloorDamage = parameters['basicFloorDamage'] ?? 1;
+
     //Monkey patch Game_Actor to remove map damage flashing and add a sound instead
     Game_Actor.prototype.performMapDamage = () => {
         if (!$gameParty.inBattle()) {
@@ -18,6 +28,6 @@
 
     //Override hard-coded floor damage of 10 per step to be just 1 instead
     Game_Actor.prototype.basicFloorDamage = () => {
-        return 1;
+        return basicFloorDamage;
     };
 })();
